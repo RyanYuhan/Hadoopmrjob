c8b22370fb4fa93c7b8630588ab9a60e7cc12b85
RyanYuhan <44513080+RyanYuhan@users.noreply.github.com>
2019-10-03 22:47:50 -0400

    Update Mapper_2.py

--- Mapper_2.py
+++ Mapper_2.py
@@ -2,14 +2,16 @@
 # --*-- coding:utf-8 --*--
 import re
 import sys
-
-par = raw_input('The time you want, the patter is num-num')
-par = par.strip()
-par = par.split()
-
+# find the date
+para = re.compile('\@\@(?P<para1>\d\d*)\-(?P<para2>\d\d*)\@\@')
+for l in sys.stdin:
+    temp =para.search(l)
+para_1 = para.group('para1')
+para_2 = para.group('para2')
+# iterate the file for mapper
 pat = re.compile('(?P<ip>\d+.\d+.\d+.\d+).*?\d{4}:(?P<hour>\d{2}):\d{2}.*? ')
 for line in sys.stdin:
     match = pat.search(line)
     if match:
-        if match.group('hour') in list(range(int(par[0]), int(par[1])+1)):
+        if match.group('hour') in list(range(int(para_1), int(para_2)+1)):
             print '%s\t%s' % (match.group('ip'), 1)
