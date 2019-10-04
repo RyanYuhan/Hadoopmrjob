3bcc8e277bb165bd95c51fd27ca61fc93d4d6230
RyanYuhan <44513080+RyanYuhan@users.noreply.github.com>
2019-10-03 21:40:06 -0400

    Update and rename run2.sh to run_2.sh

--- run_2.sh
+++ run_2.sh
@@ -5,7 +5,7 @@ bash /mapreduce-test/start.sh
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /lab_1/input/
 /usr/local/hadoop/bin/hdfs dfs -copyFromLocal /mapreduce-test/mapreduce-test-data/access.log /lab_1/input/
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.9.2.jar \
--file /Hadoopmrjob/Mapper1.py -mapper /Hadoopmrjob/Mapper1.py \
+-file /Hadoopmrjob/Mapper_2.py -mapper /Hadoopmrjob/Mapper_2.py \
 -file /Hadoopmrjob/Reducer.py -reducer /Hadoopmrjob/Reducer.py \
 -input /lab_1/input/* -output /lab_1/output/
 /usr/local/hadoop/bin/hdfs dfs -cat /lab_1/output/part-00000
