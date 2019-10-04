fbc1a05a6d350f800bfeb9c3a8db5717d39f8967
RyanYuhan <44513080+RyanYuhan@users.noreply.github.com>
2019-10-03 23:22:08 -0400

    Update Mapper_2.py

--- Mapper_2.py
+++ Mapper_2.py
@@ -2,16 +2,16 @@
 # --*-- coding:utf-8 --*--
 import re
 import sys
-# find the date
+
 para = re.compile('\@\@(?P<para1>\d\d*)\-(?P<para2>\d\d*)\@\@')
-for l in sys.stdin:
-    temp =para.search(l)
-para_1 = temp.group('para1')
-para_2 = temp.group('para2')
-# iterate the file for mapper
 pat = re.compile('(?P<ip>\d+.\d+.\d+.\d+).*?\d{4}:(?P<hour>\d{2}):\d{2}.*? ')
 for line in sys.stdin:
-    match = pat.search(line)
-    if match:
-        if match.group('hour') in list(range(int(para_1), int(para_2)+1)):
-            print '%s\t%s' % (match.group('ip'), 1)
+    temp = para.search(line)
+    if temp:
+        para_1 = temp.group('para1')
+        para_2 = temp.group('para2')
+    else:
+        match = pat.search(line)
+        if match:
+            if match.group('hour') in list(range(int(para_1), int(para_2)+1)):
+                print '%s\t%s' % (match.group('ip'), 1)
