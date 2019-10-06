f9a156f68c26342218ccb23530388ff472e43eba
RyanYuhan <44513080+RyanYuhan@users.noreply.github.com>
2019-10-06 00:54:01 -0500

    Update Mapper_2.py

--- Mapper_2.py
+++ Mapper_2.py
@@ -8,9 +8,9 @@ pat = re.compile('(?P<ip>\d+.\d+.\d+.\d+).*?\d{4}:(?P<hour>\d{2}):\d{2}.*? ')
 for line in sys.stdin:
     temp = para.search(line)
     if temp:
-        para_1 = temp.group('para1')
-        para_2 = temp.group('para2')
+        para_1 = int(temp.group('para1'))
+        para_2 = int(temp.group('para2'))
     else:
         match = pat.search(line)
-        if match and int(match.group('hour')) >= int(para_1) and int(match.group('hour')) <= int(para_2):
+        if match and int(match.group('hour')) >= para_1 and int(match.group('hour')) <= para_2:
             print ('%s\t%s' % (match.group('ip'), 1))
