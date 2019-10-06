4984a8de049b6da22580394ebb862512f8488a68
RyanYuhan <44513080+RyanYuhan@users.noreply.github.com>
2019-10-05 23:45:27 -0500

    Update run_2.sh

--- run_2.sh
+++ run_2.sh
@@ -4,9 +4,11 @@ bash /mapreduce-test/start.sh
 /usr/local/hadoop/bin/hdfs dfs -rm -r /lab_1/output/
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /lab_1/input/
 cp /mapreduce-test/mapreduce-test-data/access.log /mapreduce-test/mapreduce-test-data/access_tem.log
-echo -n "Please enter the start time\n"
+echo -n "Please enter the start time"
+echo -n ""
 read start_time
-echo -n "Please enter the end time\n"
+echo -n "Please enter the end time"
+echo -n ""
 read end_time
 sed -i "1i @@$start_time-$end_time@@" /mapreduce-test/mapreduce-test-data/access_tem.log
 /usr/local/hadoop/bin/hdfs dfs -copyFromLocal /mapreduce-test/mapreduce-test-data/access_tem.log /lab_1/input/
@@ -15,7 +17,7 @@ sed -i "1i @@$start_time-$end_time@@" /mapreduce-test/mapreduce-test-data/access
 -file /Hadoopmrjob/Reducer.py -reducer /Hadoopmrjob/Reducer.py \
 -input /lab_1/input/* -output /lab_1/output/
 /usr/local/hadoop/bin/hdfs dfs -cat /lab_1/output/part-00000
-#/usr/local/hadoop/bin/hdfs dfs -rm -r /lab_1/input/
-#/usr/local/hadoop/bin/hdfs dfs -rm -r /lab_1/output/
-#rm /mapreduce-test/mapreduce-test-data/access_tem.log
+/usr/local/hadoop/bin/hdfs dfs -rm -r /lab_1/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /lab_1/output/
+rm /mapreduce-test/mapreduce-test-data/access_tem.log
 bash /mapreduce-test/stop.sh
