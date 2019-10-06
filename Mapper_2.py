e45a88236357a586667753a58fad77d1e223079a
RyanYuhan <44513080+RyanYuhan@users.noreply.github.com>
2019-10-06 01:28:39 -0500

    Update Mapper_2.py

--- Mapper_2.py
+++ Mapper_2.py
@@ -2,15 +2,17 @@
 # --*-- coding:utf-8 --*--
 import re
 import sys
+import codecs
+sin = codecs.getreader('latin-1')(sys.stdin)
 
 para = re.compile('\@\@(?P<para1>\d\d*)\-(?P<para2>\d\d*)\@\@')
 pat = re.compile('(?P<ip>\d+.\d+.\d+.\d+).*?\d{4}:(?P<hour>\d{2}):\d{2}.*? ')
-for line in sys.stdin:
+for line in sin:
     temp = para.search(line)
     if temp:
         para_1 = int(temp.group('para1'))
         para_2 = int(temp.group('para2'))
     else:
         match = pat.search(line)
-        if match and int(match.group('hour')) >= para_1 and int(match.group('hour')) <= para_2:
-            print ('%s\t%s' % (match.group('ip'), 1))
+        if match and int(match.group('hour')) >= int(para_1) and int(match.group('hour')) <= int(para_2):
+            print '%s\t%s' % (match.group('ip'), 1)
