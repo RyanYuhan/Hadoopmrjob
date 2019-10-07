5ec3cc512f069246a6b12def2baf4b0c565602b3
RyanYuhan <44513080+RyanYuhan@users.noreply.github.com>
2019-10-07 16:15:51 -0400

    Update Mapper_2.2_wordcount.py

--- Mapper_2.2_wordcount.py
+++ Mapper_2.2_wordcount.py
@@ -3,7 +3,7 @@ import sys
 if len(sys.argv) != 2:
     print('You can only print 1 parameter')
     sys.exit()
-if len(sys.argv[1]) != 1:
+if len(str(sys.argv[1])) != 1:
     print('You can just print one letter!')
     sys.exit()
 initial = sys.argv[1]
@@ -11,7 +11,5 @@ for line in sys.stdin:
     line = line.strip()
     words = line.split()
     for word in words:
-        if word.startswith(initial):
+        if word[0] == str(initial):
             print "%s\t%s" % (word, 1)
-        else:
-            pass
