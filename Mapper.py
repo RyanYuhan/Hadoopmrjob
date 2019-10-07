ff68b703743b2c0d34b5d11d902740c6c203d5ab
RyanYuhan <44513080+RyanYuhan@users.noreply.github.com>
2019-10-07 14:10:12 -0400

    Update Mapper.py

--- Mapper.py
+++ Mapper.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python
 # --*-- coding:utf-8 --*--
-import re
 import sys
+import re
 
 pat = re.compile('(?P<ip>\d+.\d+.\d+.\d+).*?\d{4}:(?P<hour>\d{2}):\d{2}.*? ')
 for line in sys.stdin:
-    match = pat.search(line)
-    if match:
-        print '%s\t%s' % ('[' + match.group('hour') + ':00' + ']' + match.group('ip'), 1)
+    try:
+        match = pat.search(line)
+        if match:
+            print '%s\t%s' % (match.group('hour'), match.group('ip'))
+    except:
+        pass
