2e0147310f23bb3a33e8868fa16faba32f8bce32
RyanYuhan <44513080+RyanYuhan@users.noreply.github.com>
2019-10-07 15:37:14 -0400

    Update run_2.2_grep.sh

--- run_2.2_grep.sh
+++ run_2.2_grep.sh
@@ -6,7 +6,7 @@ bash /mapreduce-test/start.sh
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /lab_1/input/
 echo -e "Please enter the word you want to count\n"
 read word
-/usr/local/hadoop/bin/hdfs dfs -copyFromLocal /mapreduce-test/mapreduce-test-data/access_tem.log /lab_1/input/
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal /mapreduce-test/mapreduce-test-data/test.txt /lab_1/input/
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.9.2.jar \
 -file /Hadoopmrjob/Mapper_2.2_grep.py -mapper "/Hadoopmrjob/Mapper_2.2_grep.py $word" \
 -file /Hadoopmrjob/Reducer_2.2_grep.py -reducer /Hadoopmrjob/Reducer_2.2_grep.py \
