4ad7a65ed30baae6810337a4bab28106d9910e1b
RyanYuhan <44513080+RyanYuhan@users.noreply.github.com>
2019-10-07 15:22:35 -0400

    Update run_2.sh

--- run_2.sh
+++ run_2.sh
@@ -3,12 +3,10 @@ bash /mapreduce-test/start.sh
 /usr/local/hadoop/bin/hdfs dfs -rm -r /lab_1/input/
 /usr/local/hadoop/bin/hdfs dfs -rm -r /lab_1/output/
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /lab_1/input/
-cp /mapreduce-test/mapreduce-test-data/access.log /mapreduce-test/mapreduce-test-data/access_tem.log
 echo -e "Please enter the start time\n"
 read start_time
 echo -e "Please enter the end time\n"
 read end_time
-#sed -i "1i @@$start_time-$end_time@@" /mapreduce-test/mapreduce-test-data/access_tem.log
 /usr/local/hadoop/bin/hdfs dfs -copyFromLocal /mapreduce-test/mapreduce-test-data/access_tem.log /lab_1/input/
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.9.2.jar \
 -file /Hadoopmrjob/Mapper_2.py -mapper "/Hadoopmrjob/Mapper_2.py $start_time $end_time" \
@@ -17,5 +15,4 @@ read end_time
 /usr/local/hadoop/bin/hdfs dfs -cat /lab_1/output/part-00000
 /usr/local/hadoop/bin/hdfs dfs -rm -r /lab_1/input/
 /usr/local/hadoop/bin/hdfs dfs -rm -r /lab_1/output/
-rm /mapreduce-test/mapreduce-test-data/access_tem.log
 bash /mapreduce-test/stop.sh
