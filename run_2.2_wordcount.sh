c6a10cd7290ac68a62bf52befcc72e1db925df5f
RyanYuhan <44513080+RyanYuhan@users.noreply.github.com>
2019-10-07 15:43:37 -0400

    Create run_2.2_wordcount.sh

---
+++ run_2.2_wordcount.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+bash /mapreduce-test/start.sh
+/usr/local/hadoop/bin/hdfs dfs -rm -r /lab_1/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /lab_1/output/
+/usr/local/hadoop/bin/hdfs dfs -mkdir -p /lab_1/input/
+echo -e "Please enter the initial you want to count:\n"
+read initial
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal /mapreduce-test/mapreduce-test-data/test.txt /lab_1/input/
+/usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.9.2.jar \
+-file /Hadoopmrjob/Mapper_2.2_grep.py -mapper "/Hadoopmrjob/Mapper_2.2_grep.py $initial" \
+-file /Hadoopmrjob/Reducer_2.2_grep.py -reducer /Hadoopmrjob/Reducer_2.2_grep.py \
+-input /lab_1/input/* -output /lab_1/output/
+/usr/local/hadoop/bin/hdfs dfs -cat /lab_1/output/part-00000
+/usr/local/hadoop/bin/hdfs dfs -rm -r /lab_1/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /lab_1/output/
+bash /mapreduce-test/stop.sh
