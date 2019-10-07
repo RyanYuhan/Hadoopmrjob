0d6b7f905c7c2555a92ba924bf9a0905a4e419ec
RyanYuhan <44513080+RyanYuhan@users.noreply.github.com>
2019-10-07 15:51:59 -0400

    Update Mapper_2.py

--- Mapper_2.py
+++ Mapper_2.py
@@ -4,8 +4,10 @@ import sys
 
 if len(sys.argv) < 3:
     print('Your should input 3 parameters')
+    sys.exit()
 if int(sys.argv[1]) < 0 or int(sys.argv[2]) > 17:
     Print('The parameters are out of range')
+    sys.exit()
 start_time = sys.argv[1]
 end_time = sys.argv[2]
 pat = re.compile('(?P<ip>\d+.\d+.\d+.\d+).*?\d{4}:(?P<hour>\d{2}):\d{2}.*? ')
