1c3a035c03285ad412e41a4d0d3bf3b90e624621
RyanYuhan <44513080+RyanYuhan@users.noreply.github.com>
2019-10-07 16:00:32 -0400

    Update run_2.1.sh

--- run_2.1.sh
+++ run_2.1.sh
@@ -3,9 +3,9 @@ bash /mapreduce-test/start.sh
 /usr/local/hadoop/bin/hdfs dfs -rm -r /lab_1/input/
 /usr/local/hadoop/bin/hdfs dfs -rm -r /lab_1/output/
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /lab_1/input/
-echo -e "Please enter the start time\n"
+echo -e "Please enter the start time:\n"
 read start_time
-echo -e "Please enter the end time\n"
+echo -e "Please enter the end time:\n"
 read end_time
 /usr/local/hadoop/bin/hdfs dfs -copyFromLocal /mapreduce-test/mapreduce-test-data/access.log /lab_1/input/
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.9.2.jar \
