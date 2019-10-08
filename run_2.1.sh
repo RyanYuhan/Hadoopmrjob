410169c5d67e11ee3d56d124c68f0cc5a9008bf8
RyanYuhan <44513080+RyanYuhan@users.noreply.github.com>
2019-10-07 21:49:00 -0400

    Update run_2.1.sh

--- run_2.1.sh
+++ run_2.1.sh
@@ -9,8 +9,8 @@ echo -e "Please enter the end time:\n"
 read end_time
 /usr/local/hadoop/bin/hdfs dfs -copyFromLocal /mapreduce-test/mapreduce-test-data/access.log /lab_1/input/
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.9.2.jar \
--file /Hadoopmrjob/Mapper_2.py -mapper "/Hadoopmrjob/Mapper_2.py $start_time $end_time" \
--file /Hadoopmrjob/Reducer.py -reducer /Hadoopmrjob/Reducer.py \
+-file /Hadoopmrjob/Mapper_2.py -mapper "/Hadoopmrjob/Map_2.py $start_time $end_time" \
+-file /Hadoopmrjob/Reducer.py -reducer /Hadoopmrjob/Reduce_2.py \
 -input /lab_1/input/* -output /lab_1/output/
 /usr/local/hadoop/bin/hdfs dfs -cat /lab_1/output/part-00000
 /usr/local/hadoop/bin/hdfs dfs -rm -r /lab_1/input/
